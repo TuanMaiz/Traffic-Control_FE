db254ae5c1bf927f566904a2732a67546065b136
TuanMaiz <tuanmai1509work@gmail.com>
2022-12-26 11:18:59 +0700

    some change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { io } from 'socket.io-client'
 import { useState, useEffect } from 'react';
-import { Buffer } from 'buffer';
-import utf8 from 'utf8'
+// import { Buffer } from 'buffer';
+// import utf8 from 'utf8'
 const socket = io.connect('http://localhost:3001')
 console.log(socket);
 
@@ -10,22 +10,16 @@ console.log(socket);
 function App() {
   const [data, setData] = useState([])
   useEffect(() => {
-    // socket.on("on-chat-react", (dataReceived) => {
-    //   setData(dataReceived)
-    // })
-    socket.on("on-frame-react", (frameReceived) => { //frame receive dang bi sai dinh dang
+    socket.on("on-packet-react", (frameReceived) => { //frame receive dang bi sai dinh dang
       console.log(frameReceived);
-      // let base64ImageString = Buffer.from(frameReceived.frame, 'binary').toString('base64')
-      // let imageURL = utf8.encode(base64ImageString)
       setData(frameReceived.frame)
-      // setData(imageURL)
     })
   })
 
   console.log(data)
   return (
     <div className="App">
-      <h1>This is test socket app</h1>
+      <h1>Stream frame:</h1>
         <img className='frame' src={`data:image/jpg;base64,${data}`} alt=""/>
     </div>
   );
